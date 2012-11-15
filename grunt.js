ef10104e242aa55c26d99926493f732eb3ec6128
Spencer Markowski <spencer@theablefew.com>
2012-11-15 13:40:14 -0500

    Add grunt and package.json

--- grunt.js
+++ grunt.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 
-  var src_files = [ 'src/LICENSE'
-                   ,'src/html2canvas-pre.txt'
+  var src_files = [ 
+                   '<banner:meta.banner>'
                    ,'src/Core.js'
                    ,'src/Generate.js'
                    ,'src/Parse.js'
@@ -9,16 +9,40 @@ module.exports = function(grunt) {
                    ,'src/Queue.js'
                    ,'src/Renderer.js'
                    ,'src/Util.js'
-                   ,'Canvas.js'
-                   ,'html2canvas-post.txt'
+                   ,'renderers/Canvas.js'
                   ];
 
   grunt.initConfig({
+    pkg: '<json:package.json>',
+    // Meta
+    meta: {
+      banner: '/*\n\t<%= pkg.name %> - v<%= pkg.version %> - ' +
+              '<%= grunt.template.today("yyyy-mm-dd") %> ' + 
+              '\n\t<%= pkg.author %>' +
+              '\n*/'
+    },
+    // Build
     concat: {
       dist: {
         src: src_files,
         dest: 'build/html2canvas.js'
       }
+    },
+    // Min
+    min: {
+      dist: {
+        src: src_files,
+        dest: 'build/html2canvas.min.js',
+        seperator: ';'
+      }
+    },
+    // Clean
+    clean: {
+      folder: "build/"
     }
   });
+
+
+  grunt.loadNpmTasks('grunt-clean');
+  
 }
